b4b753b816bc7cf3f80cfaa17597420cf6c52283
Dmitrii Seletkov <uwgdz@student.kit.edu>
2022-06-20 17:42:04 +0200

    final version of local react app

--- E2/sda_matcher/src/components/Matcher.js
+++ E2/sda_matcher/src/components/Matcher.js
@@ -2,11 +2,6 @@ import React, { Component, useState } from "react";
 import { Modal, Button, Carousel, Col, Row, Container, Form } from "react-bootstrap";
 
 
-
-
-var images;
-var titel;
-
 var artistList = [{ 'dateiname': 'smf_aug_xxx_01573_003.jpg', 'titel': 'Weibliches Bildnis', 'anzeigeame': 'R. Max Seemann' },
 { 'dateiname': 'smf_aug_xxx_03611_003.jpg', 'titel': 'Selbstbildnis', 'anzeigeame': 'Julius Siegfried Uetz' },
 { 'dateiname': 'smf_aug_xxx_03587_003.jpg', 'titel': 'Brustbild eines jungen Mädchens', 'anzeigeame': 'Julius Siegfried Uetz' },
@@ -86,18 +81,20 @@ var artistList = [{ 'dateiname': 'smf_aug_xxx_01573_003.jpg', 'titel': 'Weiblich
 { 'dateiname': 'smf_aug_xxx_2017-011_003.jpg', 'titel': 'Studie eines italienischen Mädchens', 'anzeigeame': 'Franz Xaver Winterhalter' },
 { 'dateiname': 'smf_aug_xxx_2019-100_002.jpg', 'titel': '"Der Trommler"', 'anzeigeame': 'Anton Küßwieder' }];
 
-//var backendList = "";
-function refreshPage(obj) {
-    obj.getAllImagesInList()
-    //window.location.reload(false);
-  }
-function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds){
-      break;
-    }
-  }
+function getStyleTransferAndDownload(style, obj, reader) {
+    fetch(`/getstyletransferanddownload/${style}`)
+        .then(response => response.blob())
+        .then(imageBlob => {
+            console.log("Image Blob")
+            const imageObjectURL = URL.createObjectURL(imageBlob);
+            reader.readAsDataURL(imageBlob);
+            obj.setState({ profileImg: reader.result })
+            reader.onload = () => {
+                if (reader.readyState === 2) {
+                    obj.setState({ profileImg: reader.result })
+                }
+            }
+  });
 }
 
 
@@ -112,34 +109,40 @@ function download(obj, reader) {
             obj.setState({ profileImg: reader.result })
         });
 }
+
 function downloadPic(obj, file, reader) {
     const imageUrl = `/downloadpic/${file}`;
+    var imageBlobAus;
     fetch(imageUrl)
         .then(response => response.blob())
         .then(imageBlob => {
-            // Then create a local URL for that image and print it
             const imageObjectURL = URL.createObjectURL(imageBlob);
             reader.readAsDataURL(imageBlob);
             obj.setState({ profileImg: reader.result })
+            reader.onload = () => {
+                if (reader.readyState === 2) {
+                    obj.setState({ profileImg: reader.result })
+                }
+            }
+            imageBlobAus = imageBlob
         });
+    return imageBlobAus;
 }
-//array mit dateinamen
+
+
 function downloadQuery() {
     var r = fetch(`/downloadquery`)
         .then(function (response) {
             return response.json()
         })
         .then(function (data) {
-            //console.log("DOWNLOAD QUERY FUNCTION")
-            //console.log(data.result)
-
-            //obj.state.backendList = data.result
             return data.result
         }
         )
     return r
 }
-//array mit dateinamen
+
+
 function getClustering(input) {
     fetch(`/getclustering/${input}`)
         .then(function (response) {
@@ -174,70 +177,22 @@ class Matcher extends Component {
 
     }
 
-    /*state = {
-        isOpen: false,
-        profileImg: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
-
-    };*/
 
     handleClose = () => this.setState({ show: false });
-    _fetchDate = async (obj, reader) => {
-                //await this.getStyleTransfer(this.state.matchImage)
-                //download(obj, reader)
-                reader.onload = () => {
-                if (reader.readyState === 2) {
-                    obj.setState({ profileImg: reader.result })
-                }
-        }
-            }
+
 
     handleShow = () => this.setState({ show: true }, () => {
-        //var filename = getStyleTransfer('input.png', 'style.png'); 
-        //this.getAllImagesInList();
-        //console.log(images);
-        //var filename = "input.png_styled.jpg"
         const reader = new FileReader();
         if (this.state.matchImage !== undefined) {
-
-
-            //this._fetchDate(this, reader).then(
-                //() => {
-                //    //console.log("STYLE TRANSFER DONE")
-                //    //sleep(5000)
-                //    download(this, reader)
-                //    reader.onload = () => {
-                //        if (reader.readyState === 2) {
-                //    this.setState({ profileImg: reader.result })
-                //}
-                //    }
-                //})
-            //download(this, reader)
-            console.log("STATE MATCH IMAGE")
-            console.log(this.state.matchImage)
-            var filename = "rsz_dima.png" + this.state.matchImage + "_styled.jpg"
-            downloadPic(this, filename, reader)
-            reader.onload = () => {
-                      if (reader.readyState === 2) {
-                    this.setState({ profileImg: reader.result })
-                }
-                    }
+           getStyleTransferAndDownload(this.state.matchImage, this, reader)
         }
-
-
-        //sleep(10000)
-        //download(this, reader)
-
-        //download(this, reader)
     });
 
     openModal = () => this.setState({ isOpen: true });
     closeModal = () => this.setState({ isOpen: false });
 
     getAllImagesInList() {
-
-        //artistList.map((item, index) => { this.state.imagePaths.push('./croppedFotos/' + item.dateiname) });
         downloadQuery(this).then( (result) => {
-            //console.log(result)
             this.state.backendList = result
             this.stateChange(result);
             console.log("DOWNLOAD QUERY")
@@ -245,46 +200,16 @@ class Matcher extends Component {
             this.state.backendList.map((item, index) => { this.state.imagePaths.push('./croppedFotos/' + item) })
             console.log("IMAGE PATHS")
             console.log(this.state.imagePaths)
-            //window.location.reload(false)
             }
-        //sleep(2000)
-        //backendList = backendList[0]
-        //console.log(this.state.imagePaths);
         )
-        //window.location.reload(false)
     }
     handleSelect(index, e) {
-        //console.log("HANDALE SELECT")
-        //console.log(this.state.backendList)
         this.state.matchImage = this.state.backendList[index]
-        //this.state.matchImage = this.state.image[index]
         console.log("handleSelect")
         console.log(this.state.matchImage)
         var artist = artistList.find(artist => artist.dateiname === this.state.matchImage);
         this.state.titel = artist.titel;
         this.state.anzeigeame = artist.anzeigeame
-    }
-
-    imageHandler = (e) => {
-        const reader = new FileReader();
-        reader.onload = () => {
-            if (reader.readyState === 2) {
-                this.setState({ profileImg: reader.result })
-            }
-        }
-        //download(this, reader)
-        //reader.readAsDataURL(e.target.files[0])
-    };
-
-    getStyleTransfer(style) {
-        console.log("GET STYLE TRANSFER")
-        fetch(`/getstyletransfer/${style}`)
-        .then(function (response) {
-            return response.text();
-        }).then(function (text) {
-            console.log('GET response text:');
-            console.log(text);
-        });
     }
 
     stateChange = (f) => {
@@ -294,8 +219,6 @@ class Matcher extends Component {
     this.setState({
       backendList: list,
     });
-    //document.getElementsByTagName("p")[0].innerHTML = artistList.dateiname(this.state.matchImage).titel;
-
   }
 
     render() {
@@ -356,10 +279,6 @@ class Matcher extends Component {
                                 <Form.Label>Cool Match!</Form.Label>
                                 <p>
                                     {this.state.titel}
-
-
-                                    {/*{artistList.dateiname(this.state.matchImage).titel}
-                                    {artistList.dateiname(this.state.matchImage).anzeigeame}*/}
                                 </p>
                                 <p>{this.state.anzeigeame}</p>
                             </Row>
